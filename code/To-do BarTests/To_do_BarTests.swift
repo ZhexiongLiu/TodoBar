2f43c879f31ebdd304b37fd4c3e6fb9c7b3b29f9
ZhexiongLiu <macodreu@gmail.com>
2020-03-01 17:53:22 -0500

    Initial commit

---
+++ code/To-do BarTests/To_do_BarTests.swift
@@ -0,0 +1,34 @@
+//
+//  To_do_BarTests.swift
+//  To-do BarTests
+//
+//  Created by Zhexiong Liu on 3/1/20.
+//  Copyright © 2020 Zhexiong Liu. All rights reserved.
+//
+
+import XCTest
+@testable import To_do_Bar
+
+class To_do_BarTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
